3af92630eb73bdb8ce72549de50672b598d8d038
Mario0051 <windowsmario64@gmail.com>
2021-09-10 23:40:00 +0000

    Update and rename ExampleMixin.java to ServerChunkManagerMixin.java

--- src/main/java/net/fabricmc/example/mixin/ServerChunkManagerMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ServerChunkManagerMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin({ServerChunkManager.class})
-public class ExampleMixin {
+public class ServerChunkManagerMixin {
   @Inject(method = {"getTotalChunksLoadedCount"}, at = {@At("RETURN")}, cancellable = true)
   private void onGetTotalChunksLoadedCount(CallbackInfoReturnable<Integer> cir) {
     cir.setReturnValue(Integer.valueOf(441));
